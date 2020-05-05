9bf25bbc3d89e35268971eab7f6b51ae91aa3fe2
AftoflBIG5 <AftoflBIG5@gmail.com>
2020-05-05 09:18:51 -0800

    first test

---
+++ client/src/App.js
@@ -0,0 +1,46 @@
+import React , { Component } from 'react';
+import { BrowserRouter , Route} from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from './actions';
+
+import Header from './components/Dynamic/Header';
+import Landing from './components/Dynamic/Landing';
+import SurveyNew from './components/surveys/SurveyNew';
+import Dashboard from './components/Dynamic/Dashboard';
+
+class App extends Component {
+  componentDidMount() {
+    this.props.fetchUser();
+  }
+
+  render() {
+    return (
+      <div>
+      <BrowserRouter>
+        <div className="container">
+            <header className="header">
+            Header Part
+            <Header/>
+            </header>
+            <div class="content">
+              <nav className="sidebar">
+                Navigation
+              </nav>
+              <main className="hotel-view">
+                Hotel View
+              </main>
+            </div>
+
+            {/* <Header />
+            <Route exact path="/"   component={Landing} />
+            <Route exact path="/surveys"  component={Dashboard} />
+            <Route exact path="/surveys/new"  component={SurveyNew} /> */}
+        </div>
+      </BrowserRouter>
+      </div>
+     
+    );
+  }
+}
+//connect method links all actions to the component as Props. so you can reference them like props.
+export default connect(null, actions)(App);
---
+++ client/src/components/Dynamic/Header.js
@@ -0,0 +1,91 @@
+import React , { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import Payments from './Payments';
+import '../../res/css/style.css';
+
+import logo from '../../res/img/logo.png';
+import userPhoto from "../../res/img/user.jpg";
+import sprite from "../../res/img/sprite.svg";
+
+class Header extends Component {
+    renderContent() {
+        switch (this.props.auth) {
+            case null: 
+            return ;
+            case false:
+                return (<li><a href="/auth/google">continue with Google</a></li>);
+            default:
+                return [<li key="1"><Payments /></li>, <li key="2">Credits: {this.props.auth.credits}</li>,<li key="3"><a href="/api/logout">Log out</a></li>];
+        }
+    }
+
+// which makes this reusable component for other views
+
+    render() {
+        // console.log(this.props.auth);
+        // console.log(this.props);
+        return (
+          <div>
+            <div className="navbar">
+              {/* <Link to={this.props.auth ? '/surveys' : '/'} className="navbar__brand">
+                        AftoflBIG5
+                    </Link> */}
+
+              <img src={logo} alt="trillo logo" className="logo" />
+              <form action="#" className="search">
+                <input
+                  type="text"
+                  className="input"
+                  placeholder="Search Here"
+                />
+                <button className="search__button">
+                  <svg className="search__icon">
+                    <use
+                      href={sprite + "#icon-magnifying-glass"}
+                      className="search__icon"
+                    />
+                  </svg>
+                </button>
+              </form>
+              <nav className="user-nav">
+                <div className="user-nav__icon-box">
+                  <svg className="user-nav__icon">
+                    <use href={sprite + "#icon-bookmark"} />
+                  </svg>
+                  <span className="user-nav__notification">7</span>
+                </div>
+                <div className="user-nav__iconbox">
+                  <svg className="user-nav__icon">
+                    <use href={sprite + "#icon-chat"} />
+                  </svg>
+                  <span className="user-nav__notification">5</span>
+                </div>
+                <div className="user-nav__user">
+                  <img
+                    src={userPhoto}
+                    alt="user photo"
+                    className="user-nav__user-photo"
+                  />
+                  <span className="user-nav__user-name">Aftofl</span>
+                </div>
+              </nav>
+            </div>
+
+            {/* <ul className="right"></ul> */}
+
+            {/* <a href="/auth/google">Login with Google</a> */}
+            {/* {this.renderContent()} */}
+          </div>
+        );
+    }
+}
+
+// receiving all states from Redux store.
+function mapStateToProps({ auth }) {
+    /* ES17 version - when key and value are the same */ 
+    return { auth };
+    // vanilla version :: return { auth: auth };
+}
+
+export default connect(mapStateToProps)(Header);
